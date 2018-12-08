6ad65fcf2f67e7ab35470d8e27e30b60c3197833
Stefan Jakimovski <s.jakimovski94@yahoo.com>
2018-12-08 16:27:38 -0600

    MessagesRoute.js

---
+++ server/routes/MessagesRoute.js
@@ -0,0 +1,11 @@
+let express =  require("express");
+const router = express.Router();
+let {list,show,create,update,remove} = require( "../controllers/MessagesController");
+
+router.get("/messages", list);
+router.get("/messages/:id", show);
+router.post("/messages", create);
+router.put("/messages/:id", update);
+router.delete("/messages/:id", remove);
+
+module.exports =  router;
